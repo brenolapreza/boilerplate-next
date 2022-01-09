8341ed31c4143191e79cd687879f4b1e69fc2c20
brenolapreza <brenolapreza@gmail.com>
2022-01-09 15:37:19 -0300

    up boilerplate

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Olá, boilerplate!</h1>
+    </div>
+  )
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+}
+
+htlm {
+    font-size: 62.5%;
+}
+
+body {
+    font-family: Georgia, 'Times New Roman', Times, serif;
+}
+
+
+`
+
+export default GlobalStyles
